5cd7bc6b60c7018f7dd1690571356bb3c0fc6af2
wsfer <93054415+wsfer@users.noreply.github.com>
2023-05-24 11:13:51 -0300

    Add produto id attribute on get methods

--- src/utils/formatProduct.js
+++ src/utils/formatProduct.js
@@ -1,6 +1,7 @@
 // Função de utilidade para formatar os dados de uma query no banco de dados dos produtos
 const formatProduct = (query) => {
     return {
+        id: query.id,
         name: query.name,
         description: query.description,
         price: query.price/100,
