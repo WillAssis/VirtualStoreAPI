1c427024a7ba812d28f12ef83e03b5258b32ceec
wsfer <93054415+wsfer@users.noreply.github.com>
2023-06-01 14:20:15 -0300

    Changes on property names of products

--- src/controller/produtoController.js
+++ src/controller/produtoController.js
@@ -17,11 +17,11 @@ export async function createProductTable() {
             `CREATE TABLE IF NOT EXISTS produto (
                 id              INTEGER         PRIMARY KEY,
                 slug            VARCHAR(100),
-                name            VARCHAR(100),
+                title           VARCHAR(100),
                 description     VARCHAR(100),
                 price           INTEGER,
                 images          VARCHAR(256),
-                featured        INTEGER         DEFAULT         0
+                destaque        INTEGER         DEFAULT         0
             )`
         );
     });
@@ -30,16 +30,16 @@ export async function createProductTable() {
 export async function insertProduto(produto) {
     return openDb().then(db => {
         return db.run(
-            `INSERT INTO produto (slug, name, description, price, images, featured)
+            `INSERT INTO produto (slug, title, description, price, images, destaque)
             VALUES (?, ?, ?, ?, ?, ?)`,
             [
                 // Lower case -> Tira caractéres inválidos -> Tira espaços duplicados -> Insere - nos espaços
-                produto.name.toLowerCase().replace(/[^a-zA-Z0-9 ]/g, '').replace(/\s+/g, ' ').replace(/[ ]/g, '-'),
-                produto.name,
+                produto.title.toLowerCase().replace(/[^a-zA-Z0-9 ]/g, '').replace(/\s+/g, ' ').replace(/[ ]/g, '-'),
+                produto.title,
                 produto.description,
                 Math.round(produto.price*100),
                 JSON.stringify(produto.images),
-                produto.featured
+                produto.destaque
             ]
         );
     });
@@ -58,16 +58,16 @@ export async function updateProduto(produto) {
     openDb().then(db => {
         db.run(
             `UPDATE produto
-            SET slug=?, name=?, description=?, price=?, images=?, featured=?
+            SET slug=?, title=?, description=?, price=?, images=?, destaque=?
             WHERE id=?`,
             [
                 // Lower case -> Tira caractéres inválidos -> Tira espaços duplicados -> Insere - nos espaços
-                produto.name.toLowerCase().replace(/[^a-zA-Z0-9 ]/g, '').replace(/\s+/g, ' ').replace(/[ ]/g, '-'),
-                produto.name,
+                produto.title.toLowerCase().replace(/[^a-zA-Z0-9 ]/g, '').replace(/\s+/g, ' ').replace(/[ ]/g, '-'),
+                produto.title,
                 produto.description,
                 Math.round(produto.price*100),
                 JSON.stringify(produto.images),
-                produto.featured
+                produto.destaque
             ]
         );
     });
@@ -88,7 +88,7 @@ export async function getFeaturedProdutos() {
     return openDb().then(db => {
         return db.all(
             `SELECT * FROM produto
-            WHERE produto.featured == 1;`
+            WHERE produto.destaque == 1;`
         );
     });
 }
