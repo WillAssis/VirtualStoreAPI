c92a09aa4abbd101b34b39e30058e86f24fff805
eliabev <eliabev@gmail.com>
2024-02-21 18:13:07 -0300

    Adding husky and creating first test

---
+++ src/tests/unit/controller/clientController.test.js
@@ -0,0 +1,33 @@
+const clientController = require('../../../controller/clienteController.js');
+
+describe('clientController', () => {
+    describe('createClient', () => {
+        it('should create a new client', () => {
+            // Test implementation goes here
+        });
+    });
+
+    describe('getClient', () => {
+        it('should get a client by ID', () => {
+            // Test implementation goes here
+        });
+    });
+
+    describe('updateClient', () => {
+        it('should update a client by ID', () => {
+            // Test implementation goes here
+        });
+    });
+
+    describe('deleteClient', () => {
+        it('should delete a client by ID', () => {
+            // Test implementation goes here
+        });
+    });
+
+    describe('getAllClients', () => {
+        it('should get all clients', () => {
+            // Test implementation goes here
+        });
+    });
+});
